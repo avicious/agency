dbf41afafc6f59bceaf8162fe38f089e79f4bd20
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-23 23:44:52 +0530

    Navbar updated for smaller screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,16 @@ const Navbar = ({ theme, setTheme }) => {
       <img
         src={theme === "dark" ? assets.logo_dark : assets.logo}
         className="w-32 sm:w-40"
-        alt="logo"
+        alt="agency"
       />
 
       <div className="text-gray-700 dark:text-white sm:text-sm max-sm:w-60 max-sm:pl-10 max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-primary max-sm:text-white max-sm:pt-20 flex sm:items-center gap-5 transition-all">
+        <img
+          src={assets.close_icon}
+          className="w-5 absolute right-4 top-4 sm:hidden"
+          alt="close"
+        />
+
         <a href="#" className="sm:hover:border-b">
           Home
         </a>
@@ -29,7 +35,7 @@ const Navbar = ({ theme, setTheme }) => {
           href="#contact"
           className="text-sm max-sm:hidden flex items-center gap-2 bg-primary text-white px-6 py-2 rounded-full cursor-pointer hover:scale-103 transition-all"
         >
-          Connect <img src={assets.arrow_icon} width={14} alt="" />
+          Connect <img src={assets.arrow_icon} width={14} alt="arrow right" />
         </a>
       </div>
     </div>
