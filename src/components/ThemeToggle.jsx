006e5b6bf04e991aa4c14169e4cba158463c59a4
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-24 00:11:52 +0530

    theme toggle updated for persistance

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
 import assets from "../assets/assets";
 
 const ThemeToggle = ({ theme, setTheme }) => {
+  useEffect(() => {
+    const prefersDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)",
+    ).matches;
+
+    setTheme(theme || (prefersDarkMode ? "dark" : "light"));
+  }, []);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
     <>
-      <button>
+      <button className="cursor-pointer">
         {theme === "dark" ? (
           <img
             src={assets.sun_icon}
