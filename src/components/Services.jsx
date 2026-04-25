0cbc883bc1f19a2d5e5ef142a333601bde2c75ff
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-26 00:39:29 +0530

    Title updated and added to services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,6 @@
 import { servicesData } from "../assets/assets";
 import assets from "../assets/assets";
+import Title from "./Title";
 
 const Services = () => {
   return (
@@ -11,6 +12,11 @@ const Services = () => {
         className="absolute -top-110 -left-70 -z-1 dark:hidden select-none pointer-events-none"
         src={assets.bgImage2}
         alt="gradient"
+      />
+
+      <Title
+        title="How can we help?"
+        content="From strategy to execution, we craft digital solutions that move your business forward."
       />
     </div>
   );
