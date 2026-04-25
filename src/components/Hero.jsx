8bce57b41fc5019f31c3f77891b1dec26e4842b1
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-25 23:42:11 +0530

    Hero section updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,12 @@
 const Hero = () => {
-    return (
-        <div>Hero</div>
-    );
+  return (
+    <div
+      id="hero"
+      className="flex flex-col items-center gap-6 py-20 px-4 sm:px-12 lg:px-24 xl:px-40 text-center w-full overflow-hidden text-gray-700 dark:text-white"
+    >
+      
+    </div>
+  );
 };
 
 export default Hero;
