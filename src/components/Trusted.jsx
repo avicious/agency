aba0f880cddc963145eeb4772e91b919357cb485
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-26 00:14:01 +0530

    Trusted section updated

--- src/components/Trusted.jsx
+++ src/components/Trusted.jsx
@@ -2,12 +2,17 @@ import { company_logos } from "../assets/assets";
 
 const Trusted = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center px-4 sm:px-12 lg:px-24 xl:px-40 gap-10 text-gray-700 dark:text-white/80">
       <h2 className="font-semibold">Trusted by Leading Companies</h2>
 
       <div className="flex items-center justify-center flex-wrap gap-10 m-4">
         {company_logos.map(({ id, logo }) => (
-          <img key={id} src={logo} alt={logo} />
+          <img
+            key={id}
+            src={logo}
+            alt={logo}
+            className="max-h-5 sm:max-h-6 dark:drop-shadow-xl"
+          />
         ))}
       </div>
     </div>
