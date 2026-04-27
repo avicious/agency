be36cd3dd46640d61f7875c26bc37663a5fe08fc
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-28 00:02:11 +0530

    work section finished

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,5 @@
 import Title from "./Title";
+import { workData } from "../assets/assets";
 
 const Work = () => {
   return (
@@ -10,6 +11,24 @@ const Work = () => {
         title="Our Latest Work"
         content="From strategy to execution, we craft digital solutions that move your business forward."
       />
+
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-5xl">
+        {workData.map((work) => (
+          <div
+            key={work.id}
+            className="hover:scale-102 duration-500 transition-all cursor-pointer"
+          >
+            <img
+              src={work.image}
+              alt={work.title}
+              className="w-full rounded-xl"
+            />
+
+            <h3 className="mt-3 mb-2 text-lg font-semibold">{work.title}</h3>
+            <p className="text-sm opacity-60 w-5/6">{work.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
