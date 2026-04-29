3653b0686b4e3ba0f13a0be153df4f5d72827254
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-29 19:52:49 +0530

    cleaned up css to avoid repitition

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -3,7 +3,7 @@ import { teamData } from "../assets/assets";
 
 const Teams = () => {
   return (
-    <div className="flex flex-col items-center gap-6 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-800 dark:text-white">
+    <div className="flex-wrapper pt-30 text-gray-700 dark:text-white">
       <Title
         title="Meet the team"
         content="A passionate team of digital experts dedicated to your brand's success."
