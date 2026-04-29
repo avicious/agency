be7860d3bb70b1d17547a50d80e99500bf4cae5c
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-29 19:56:27 +0530

    cleaned up css to avoid repitition

--- src/components/Trusted.jsx
+++ src/components/Trusted.jsx
@@ -2,7 +2,7 @@ import { company_logos } from "../assets/assets";
 
 const Trusted = () => {
   return (
-    <div className="flex flex-col items-center px-4 sm:px-12 lg:px-24 xl:px-40 gap-10 text-gray-700 dark:text-white/80">
+    <div className="flex-wrapper text-gray-700 dark:text-white/80">
       <h2 className="font-semibold">Trusted by Leading Companies</h2>
 
       <div className="flex items-center justify-center flex-wrap gap-10 m-4">
