afc25bf655cbce1f118cece417765a45c926c42d
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-29 19:56:56 +0530

    cleaned up css to avoid repitition

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,10 +3,7 @@ import { workData } from "../assets/assets";
 
 const Work = () => {
   return (
-    <div
-      id="work"
-      className="flex flex-col items-center gap-6 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white"
-    >
+    <div id="work" className="flex-wrapper pt-30 text-gray-700 dark:text-white">
       <Title
         title="Our Latest Work"
         content="From strategy to execution, we craft digital solutions that move your business forward."
