43996a6f58c0b7f5e911826cf071e132735c06ac
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-29 19:48:53 +0530

    cleaned up css to avoid repitition

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div
       id="hero"
-      className="flex flex-col items-center gap-6 py-20 px-4 sm:px-12 lg:px-24 xl:px-40 text-center w-full overflow-hidden text-gray-700 dark:text-white"
+      className="flex-wrapper py-20 text-center w-full overflow-hidden text-gray-700 dark:text-white"
     >
       <div className="inline-flex items-center gap-2 border border-gray-300 p-1.5 pr-4 rounded-full">
         <img className="w-20" src={assets.group_profile} alt="group profile" />
