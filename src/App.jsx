444af38ffc122967f6d4b398cf443256a6e16ca5
Shubham Kumar <shubham.kr2951@gmail.com>
2026-04-29 19:37:53 +0530

    index added to components for cleaner imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Trusted from "./components/Trusted";
-import Services from "./components/Services";
-import Work from "./components/Work";
-import Teams from "./components/Teams";
-import Contact from "./components/Contact";
+import {
+  Navbar,
+  Hero,
+  Trusted,
+  Services,
+  Work,
+  Teams,
+  Contact,
+} from "./components/index";
 
 const App = () => {
   const getTheme = localStorage.getItem("theme")
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Trusted from "./Trusted";
+import Services from "./Services";
+import Work from "./Work";
+import Teams from "./Teams";
+import Contact from "./Contact";
+
+export { Navbar, Hero, Trusted, Services, Work, Teams, Contact };
