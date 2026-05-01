dbc938ce078558cef482a201fc97370da8a2af93
Shubham Kumar <shubham.kr2951@gmail.com>
2026-05-01 23:46:01 +0530

    framer motion animation added to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion } from "motion/react";
 import ThemeToggle from "./ThemeToggle.jsx";
 import assets from "../assets/assets.js";
 
@@ -6,7 +7,12 @@ const Navbar = ({ theme, setTheme }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70">
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70"
+    >
       <img
         src={theme === "dark" ? assets.logo_dark : assets.logo}
         className="w-32 sm:w-40"
@@ -70,7 +76,7 @@ const Navbar = ({ theme, setTheme }) => {
           Connect <img src={assets.arrow_icon} width={14} alt="arrow right" />
         </a>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
