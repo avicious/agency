042395f4fc9958df7dcd3e38f61bade82853bcc4
Shubham Kumar <shubham.kr2951@gmail.com>
2026-05-02 23:46:30 +0530

    animation cleaned for teams section

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -5,8 +5,9 @@ import { teamData } from "../assets/assets";
 const Teams = () => {
   return (
     <motion.div
-      initial="hidden"
-      whileInView="visible"
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.4, delay: 0.1 }}
       viewport={{ once: true }}
       className="flex-wrapper pt-30 text-gray-700 dark:text-white"
     >
@@ -17,11 +18,7 @@ const Teams = () => {
 
       <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
         {teamData.map((team, index) => (
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.4, delay: index * 0.1 }}
-            viewport={{ once: true }}
+          <div
             key={team.id}
             className="flex max-sm:flex-col items-center gap-5 p-4 rounded-xl border border-gray-100 dark:border-gray-700 bg-white dark:bg-[#040D0C] shadow-xl shadow-gray-100 dark:shadow-white/5 hover:scale-103 transition-all duration-400"
           >
@@ -34,7 +31,7 @@ const Teams = () => {
               <h3 className="font-bold text-sm">{team.name}</h3>
               <p className="text-xs opacity-60">{team.title}</p>
             </div>
-          </motion.div>
+          </div>
         ))}
       </div>
     </motion.div>
